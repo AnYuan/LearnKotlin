5a3629acd6bb90e920f4b9d9a2fd4213bd33cc05
Anyuan Dong <donganyuan1010@gmail.com>
2020-07-09 17:07:47 +0800

    add ch2 and ch3 code

--- 03-types-operations/starter/src/types-and-operations.kt
+++ 03-types-operations/starter/src/types-and-operations.kt
@@ -29,5 +29,84 @@
  */
 
 fun main() {
+    // Type conversion
+    var integer: Int = 100
+    var decimal: Double = 12.5
+    integer = decimal.toInt()
 
+    // Operators with mixed types
+    val hourlyRate: Double = 19.5
+    val hoursWorked: Int = 10
+    val totalCost: Double = hourlyRate * hoursWorked.toDouble()
+
+    // Type inference
+    val typeInferredInt = 42
+    val actuallyDouble: Double = 3.0 // can not use 3
+
+    // Strings
+    val characterA: Char = 'a'
+    val stringDog: String = "Dog"
+    val stringDog1 = "Dog"
+
+    // concatenation
+    var message = "Hello" + " my name is "
+    val name = "Joe"
+    message += name
+    println(message)
+
+    // String templates
+    message = "Hello my name is $name!"
+    println(message)
+
+    val oneThird = 1.0 / 3.0
+    val oneThirdLongString = "One third is $oneThird as a decimal."
+    val oenThridLongString1 = "One third is ${1.0 / 3.0} as a decimal."
+
+    // Multi-line strings
+    val bigString = """
+        |You can have a string
+        |that contains multiple
+        |lines
+        |by
+        |doing this.
+    """.trimMargin()
+    println(bigString)
+
+    // Pairs and Triples
+    val coordinates: Pair<Int, Int> = Pair(2,3)
+    val coordinatesInferred = Pair(2,3)
+    val coordinatesInferred1 = 2 to 3
+    val coordinatesDoubles = Pair(2.1, 3.5)
+
+    val x1 = coordinates.first
+    val y1 = coordinates.second
+
+    val (x, y) = coordinates
+
+    val coordinates3D = Triple(2,3,1)
+    val (x3, y3, _) = coordinates3D
+
+    // Number types
+    val a: Short = 12
+    val b: Byte = 120
+    val c: Int = -10000
+    val answer = a + b + c // Answer will be an Int
+
+    // Any, Unit, and Nothing Types
+    val anyNumber: Any = 42
+    val anyString: Any = "42"
 }
+
+// Unit
+fun add(): Unit {
+    val result = 2 + 2
+    println(result)
+}
+
+// Nothing
+fun doNothingForever(): Nothing {
+    while (true) {
+
+    }
+}
+
