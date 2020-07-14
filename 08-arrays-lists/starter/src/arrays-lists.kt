dc00509ab96c554731849af21e6ca4b53db04bb0
Anyuan Dong <donganyuan1010@gmail.com>
2020-07-14 08:58:54 +0800

    add ch8 code

--- 08-arrays-lists/starter/src/arrays-lists.kt
+++ 08-arrays-lists/starter/src/arrays-lists.kt
@@ -29,5 +29,78 @@
  */
 
 fun main(args: Array<String>) {
+    // Arrays
+    val evenNumber = arrayOf(2,4,6,8)
+    val fiveFives = Array(5, { 5 }) // 5,5,5,5,5
+    val vowels = arrayOf("a", "e", "i", "o", "u")
 
+    // arrays of primitive types
+    val oddNumbers = intArrayOf(1,3,5,7)
+    val zeros = DoubleArray(4) // 0.0, 0.0, 0.0, 0.0
+    val otherOddNumbers = arrayOf(1,3,5,7).toIntArray()
+
+
+    for (arg in evenNumber) {
+        println(arg)
+    }
+
+    evenNumber.forEach { arg -> println(arg) }
+
+
+    // Lists
+    val innerPlanets = listOf("Mercury", "Venus", "Earth", "Mars")
+    val innerPlanetsArrayList = arrayListOf("Mercury", "Venus", "Earth", "Mars")
+
+    // Mutable lists
+    val outerPlanets = mutableListOf("Jupiter", "Saturn", "Uranus", "Neptune")
+    val exoPlanets = mutableListOf<String>()
+
+    // Accessing elements
+    // using properties and methods
+    val players = mutableListOf("Alice", "Bob", "Cindy", "Dan")
+    println(players.isEmpty())
+    var currentPlayer = players.first()
+    println(players.last())
+    val minPlayer = players.min()
+    minPlayer.let { println("$minPlayer will start") }
+
+    // using indexing
+    val firstPlayer = players[0]
+    val secondPlayer = players.get(1)
+    //val player = players[4] > IndexOutOfBoundsException
+
+    // using ranges to slice
+    val upcomingPlayersSlice = players.slice(1..2)
+    println(upcomingPlayersSlice.joinToString()) // > Bob, Cindy
+
+    // checking for an element
+    println(isEliminated("Bob")) // false
+    players.slice(1..3).contains("Alice") // false
+
+    // Modifying lists
+    // appending elements
+    players.add("Eli")
+    players += "Gina"
+
+    // inserting elements
+    players.add(5, "Frank")
+
+    // removing elements
+    val wasPlayerRemoved = players.remove("Gina")
+    println("It is $wasPlayerRemoved that Gina was removed")
+    val removedPlayer = players.removeAt(2) // returns the element that was removed from the list.
+    println("$removedPlayer was removed")
+
+    // Nullability and collection types
+    var nullableList: List<Int>? = listOf(1,2,3,4)
+    nullableList = null
+    var listOfNullables: List<Int?> = listOf(1,2,null,4)
+
+    var nullableListOfNullables: List<Int?>? = listOf(1,2,null,4)
+    nullableListOfNullables = null
+
+}
+
+fun isEliminated(player: String): Boolean {
+    return player !in players
 }
