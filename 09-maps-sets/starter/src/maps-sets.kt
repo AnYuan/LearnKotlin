1dd782c35f15f4ea468e42241830bf87fe742442
Anyuan Dong <donganyuan1010@gmail.com>
2020-07-20 09:53:48 +0800

    add ch9 code

--- 09-maps-sets/starter/src/maps-sets.kt
+++ 09-maps-sets/starter/src/maps-sets.kt
@@ -30,4 +30,72 @@
 
 fun main() {
 
+    // Map
+    // Creating maps
+    var yearOfBirth = mapOf("Anna" to 1990, "Brian" to 1991, "Craig" to 1992, "Donna" to 1993)
+    var namesAndScores = mutableMapOf("Anna" to 2, "Brian" to 2, "Craig" to 8, "Donna" to 6)
+    namesAndScores = mutableMapOf()
+    println(namesAndScores)
+    var pairs = HashMap<String, Int>()
+    // define its capacity
+    pairs = HashMap<String, Int>(20)
+
+
+    // Accessing values
+
+    // using the index operator
+    println(namesAndScores["Anna"])
+    namesAndScores["Greg"] // null
+
+    // using properties and methods
+    println(namesAndScores.get("Craig"))
+    namesAndScores.isEmpty()
+    namesAndScores.size
+
+    // Modifying mutable maps
+    val bobData = mutableMapOf("name" to "Bob", "profession" to "CardPlayer", "country" to "USA")
+    bobData.put("state", "CA")
+    bobData["city"] = "San Francisco"
+
+    // Updating values
+    bobData.put("name", "Bobby")
+    bobData["profession"] = "Mailman"
+    val pair = "nickname" to "Bobby D"
+    bobData += pair
+    println(bobData)
+
+    // Removing pairs
+    bobData.remove("city")
+    bobData.remove("state", "CA")
+
+    // Iterating through maps
+    for ((player, score) in namesAndScores) {
+        println("$player - $score")
+    }
+
+    // Running time for map operations
+    // Accessing elements: O(1)
+    // Inserting elements: O(1)
+    // Deleting elements: O(1)
+    // searching for an elemnt: O(1)
+
+    // For perermance-critical code, HashMap<K, V> should be used
+    // via hashMapOf(), instead of mapOf()
+
+    // Set
+    // Creating sets
+    val names = setOf("Anna", "Brian", "Craig", "Anna")
+    println(names)
+    val hashSet = HashSet<Int>()
+    // set from arrays
+    val someArray = arrayOf(1,2,3,1)
+    var someSet = mutableSetOf(*someArray)
+
+    // Accessing elements
+    println(someSet.contains(1))
+    println(4 in someSet)
+
+    // Adding and removing elements
+    someSet.add(5)
+    val removedOne = someSet.remove(1)
 }
