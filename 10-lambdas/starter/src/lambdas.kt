2ac505db3c2cf969f8f98fbbf834d7457d05e663
Anyuan Dong <donganyuan1010@gmail.com>
2020-07-20 14:08:42 +0800

    add ch10 code Lambdas basics

--- 10-lambdas/starter/src/lambdas.kt
+++ 10-lambdas/starter/src/lambdas.kt
@@ -28,6 +28,89 @@
  * THE SOFTWARE.
  */
 
-fun main() {
 
+fun addFunction(a: Int, b: Int) = a + b
+
+fun main() {
+    // Lambdas basics
+    var multiplyLambda: (Int, Int) -> Int
+    multiplyLambda = { a: Int, b: Int -> Int
+        a * b
+    }
+    val lambdaResult = multiplyLambda(4, 2)
+    // Shorthand syntax
+    multiplyLambda = {a, b -> a * b }
+
+    // it keyword
+    var doubleLambda = { a: Int -> 2 * a }
+    doubleLambda = { 2 * it }
+    val square: (Int) -> Int = { it * it }
+
+    // Lambdas as arguments
+    fun operateOnNumbers(a: Int, b: Int, operation: (Int, Int) -> Int): Int {
+        val result = operation(a, b)
+        println(result)
+        return result
+    }
+    val addLambda = { a: Int, b: Int -> a + b }
+    operateOnNumbers(4,2, operation = addLambda)
+    operateOnNumbers(4,2, operation = ::addFunction) // 6
+    operateOnNumbers(4,2, operation = { a: Int, b: Int -> a + b })
+    operateOnNumbers(4,2, {a, b -> a + b })
+    operateOnNumbers(4,2, operation = Int::plus)
+    operateOnNumbers(4, 2) { a, b -> a + b }
+
+    // Lambdas with no meaningful return value
+    var unitLambda: () -> Unit = {
+        println("Kotlin Apprentice is awesome!")
+    }
+    unitLambda()
+
+    var nothingLambda: () -> Nothing = { throw
+            NullPointerException()
+    }
+
+    // Capturing from the enclosing scope
+    var counter = 0
+    var incrementCounter = { counter += 1 }
+
+    // Custom sorting with lambdas
+    val names = arrayOf("ZZZZZZ", "BB", "A", "CCCC", "EEEEE")
+    names.sorted()
+
+    val namesByLength = names.sortedWith(compareBy {
+        -it.length
+    }) // [ZZZZZZ, EEEEE, CCCC, BB, A]
+
+    // Iterating over collections with lambdas
+    val values = listOf(1,2,3,4,5,6)
+    values.forEach {
+        println("$it: ${it * it}")
+    }
+    // Filter
+    var prices = listOf(1.5, 10.0, 4.99, 2.30, 8.19)
+    val largePrices = prices.filter {
+        it > 5.0
+    }
+    // Map
+    val salePrices = prices.map {
+        it * 0.9
+    }
+
+    val userInput = listOf("0", "11", "haha", "42")
+    val numbers = userInput.map {
+        it.toIntOrNull()
+    }
+    println(numbers)
+
+    val numbers2 = userInput.mapNotNull {
+        it.toIntOrNull()
+    }
+    println(numbers2)
+
+    // Reduce
+    var sum = prices.fold(0.0) { a, b ->
+        a + b
+    }
+    sum = prices.reduce { a, b -> a + b }
 }
